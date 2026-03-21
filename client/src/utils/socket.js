40817320916831912c0bec15c142177fdbd22b4e
anjali1353 <tiwarianjali8865@gmail.com>
2026-03-22 01:37:36 +0530

    fixed socket.js for render

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:4000', {
+const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:4000';
+
+const socket = io(SERVER_URL, {
   transports: ['websocket'],
   autoConnect: true,
 });
